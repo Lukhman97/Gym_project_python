6c673b641f463de874c9e0118aa89f473040783f
Lukhman97 <lukhmanshaik786@gmail.com>
2026-01-08 14:12:57 +0530

    added all the data

--- gymcore/accounts/models.py
+++ gymcore/accounts/models.py
@@ -1,13 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class AttendanceRecord(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
 
-class MemberProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone = models.CharField(max_length=15)
-    age = models.PositiveIntegerField()
-    membership_plan = models.CharField(max_length=50)
-    join_date = models.DateField(auto_now_add=True)
-
-    def __str__(self):
-        return self.user.username
+    class Meta:
+        unique_together = ('user', 'date')
--- gymcore/accounts/urls.py
+++ gymcore/accounts/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login/", views.login_view, name="login"),
-    path("signup/", views.signup_view, name="signup"),
-    path("logout/", views.logout_view, name="logout"),
+    path('mark_attendance/', views.mark_attendance, name='mark_attendance'),
+    path('attendance_stats/', views.attendance_stats, name='attendance_stats'),
+    path('attendance_totalstats/', views.attendance_totalstats, name='attendance_totalstats'),
+    path('attendance_totalstats_today/', views.attendance_totalstats_today, name='attendance_totalstats_today'),
 ]
