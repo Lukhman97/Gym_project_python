00eddf58f6ca429c6fe0ca04221d45803f752968
Lukhman97 <lukhmanshaik786@gmail.com>
2026-01-08 15:00:39 +0530

    yes commit

--- gymcore/gymcore/urls.py
+++ gymcore/gymcore/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('main.urls')),
-    path('',include('registration.urls')),
-    path('',include('measurements.urls')),
-    path('',include('workout_setup.urls')),
-    path('',include('attendance.urls')),
-    path('',include('memberships.urls')),
-]
 
+    path('', include('main.urls')),                     # Home / Dashboard
+    path('register/', include('registration.urls')),   # Registration
+    # path('accounts/', include('accounts.urls')),       # Create User / Login
+    path('memberships/', include('memberships.urls')), # Plans & Payments
+    path('workouts/', include('workout_setup.urls')),  # Workout plans
+    path('measurements/', include('measurements.urls')),# Body measurements
+    path('attendance/', include('attendance.urls')),   # Attendance
+]
