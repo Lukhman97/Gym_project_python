53acbd9ef3b4173d7eb888f2848f4ccc0a02916a
Lukhman97 <lukhmanshaik786@gmail.com>
2026-02-04 12:29:34 +0530

    Update ALLOWED_HOSTS for production deployment

--- gymcore/gymcore/settings.py
+++ gymcore/gymcore/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY=os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gym-project-python.onrender.com']
 
 
 # Application definition
